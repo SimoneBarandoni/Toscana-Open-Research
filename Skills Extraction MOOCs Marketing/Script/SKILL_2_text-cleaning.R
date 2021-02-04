dd73c995ec4b6375183de0c9d78981033ca85020
gianlucaruggi <73689688+gianlucaruggi@users.noreply.github.com>
2021-02-04 12:16:44 +0100

    Update SKILL_2_text-cleaning.R

--- Skills Extraction MOOCs Marketing/Script/SKILL_2_text-cleaning.R
+++ Skills Extraction MOOCs Marketing/Script/SKILL_2_text-cleaning.R
@@ -1,143 +1,133 @@
-library(readxl)
 library(rebus)
 library(tidyverse)
 library(tidytext)
-library(tm)
-
-it_model <- udpipe::udpipe_load_model("Input/italian-isdt-ud-2.4-190531.udpipe")
-
-blacklist <- read.csv2("Input/blacklist1.csv")
-
-not_ex <- read.csv2("Input/not_existing_words.csv")
-
-trigrElim <- read.csv2("Intermediate/eliminaTrigr.csv")
-
-bigrElim <- read.csv2("Intermediate/eliminaBigr.csv")
-
-vocabolario <- read.csv2("Input/dizionario-totale.csv")
-
-wrong_lemmatized <- read_xlsx("Input/wrong_lemmatized.xlsx")
 
 df_1 <- readRDS("Intermediate/df_1.rds")
 
-# FUNCTION PREP_FUN ---------------------------------------------------------------
+df_2 <- readRDS("Intermediate/df_2.rds")
 
-prep_fun = function(x) {
-  # make text lower case
-  x = str_to_lower(x)
-  # remove non-alphanumeric symbols   
-  x = str_replace_all(x, "[^&^_[:^punct:]]", " ")
-  # collapse multiple spaces
-  str_replace_all(x, "\\s+", " ")
-  # remove stopwords
-  x = removeWords(x, stopwords("italian"))
-  # collapse multiple spaces
-  str_replace_all(x, "\\s+", " ")
-}
+# SKILL COLUMN TEXT CLEANING BY RULES --------------------------------------------------
 
-# BLACKLIST, VOCABULARY AND NOT EXISTING WORDS PREPARATION ------------------------------
+# english language skills sentences cleaning
 
-# preparation of blacklist and not_existing_words lists
-blacklist <- blacklist %>% 
-  filter(in_out == 1) %>% 
-  as_tibble() %>% 
-  pull(Espressione)
+df_2_2 <- df_2 %>% 
+  mutate(skill_clean = if_else(id == 18, "", skill_clean)) #there is only one at id eighteen
 
-not_ex <- not_ex %>% 
-  group_by(lemma) %>% 
-  arrange(-in_out) %>% 
-  filter(in_out == 1 | in_out == 0) %>% 
-  ungroup() %>% 
-  distinct(lemma, .keep_all = T)
+# interrogative and exclamative sentences cleaning
 
-vocabolario <- vocabolario %>%
-  filter(!str_detect(parola, one_or_more(WRD) %R% SPC %R% one_or_more(WRD))) %>%
-  filter(!str_detect(parola, PUNCT)) %>%
-  mutate(parola = str_to_lower(parola)) %>%
-  rename(suggested_token = parola) %>%
-  select(suggested_token) %>%
-  distinct(suggested_token) %>%
-  mutate(tag = 1)
+df_2_2 <- df_2_2 %>% 
+  filter(!str_detect(skill_clean, or("\\!", "\\?")))
 
-# SENTENCES LEMMATIZATION AND CLEANING -------------------------------------
+# take the negative version of cleaned dataset in order to check the cleaning fairness
 
-df_1_1 <- df_1
+df_dirty_2 <- df_2 %>% 
+  left_join(df_2_2 %>% select(id, id_sentence) %>% mutate(tag = 1), by = c("id", "id_sentence")) %>% 
+  filter(is.na(tag))
 
+# DESCR COLUMN TEXT CLEANING BY RULES --------------------------------------------------
 
-df_lemma <- udpipe::udpipe_annotate(object = it_model,
-                                             x = iconv(df_1_1$descr),
-                                             doc_id = df_1_1$id_sentence) %>%
-  as_tibble() %>% 
-  select(doc_id, token, lemma, upos )
+# updates sentences cleaning
 
+df_1_1 <- df_1 %>% 
+  filter(!str_detect(descr_clean, whole_word(case_insensitive("aggiornament") %R% one_or_more(WRD)))) %>% 
+  filter(!str_detect(descr_clean, whole_word(case_insensitive("updat") %R% one_or_more(WRD))))
 
-df_lemma <- left_join(df_lemma,vocabolario, by = c("token" = "suggested_token")) %>%
-  mutate(lemma = if_else(!is.na(tag), token, lemma)) %>%
-  select(-tag)
-
-df_lemma <- left_join(df_lemma,wrong_lemmatized, by = "lemma") %>%
-  mutate(lemma = if_else(!is.na(correct), correct, lemma)) %>%
-  select(-correct)
-
-# for the short texts: collapse the lemmas to create a lemmatized text
-df_1_1 <- df_lemma %>% 
-  filter(!is.na(token)) %>%
-  mutate(lemma = if_else(is.na(lemma), token, lemma)) %>% 
-  left_join(not_ex[,c("lemma","proper_lemma")], by = "lemma") %>% 
-  mutate(lemma = if_else(is.na(proper_lemma),lemma,proper_lemma)) %>% 
-  group_by(doc_id) %>% 
-  summarise(testoLemm = paste(lemma, collapse = " ")) %>% 
-  ungroup()%>% 
-  mutate(doc_id = as.numeric(doc_id)) %>% 
-  arrange(doc_id)
-
-df_1_1$testoLemm <-  prep_fun(df_1_1$testoLemm)
-
-for(i in 1: nrow(df_1_1)){ # for each abstract
-  
-  print(str_c("papers done ----->  ", round(i/nrow(df_1_1), digits = 4)*100, "%"))
-  
-  for(j in 1: nrow(trigrElim)){ # for each multiword
-    df_1_1[[i,2]] <- str_replace_all(df_1_1[[i,2]], trigrElim[[j,2]], "")
-    
-  }
-  
-}
-
-for(i in 1: nrow(df_1_1)){ # for each abstract
-  
-  print(str_c("papers done ----->  ", round(i/nrow(df_1_1), digits = 4)*100, "%"))
-  
-  for(j in 1: nrow(bigrElim))
-  {
-    df_1_1[[i,2]] <- str_replace_all(df_1_1[[i,2]], bigrElim[[j,2]], "")
-  }
-  
-}
-
-# now eliminate blacklisted lemma
-df_1_1 <- df_1_1 %>% 
-  unnest_tokens(lemma,testoLemm) %>%
-  filter(!lemma %in% blacklist) %>% 
-  group_by(doc_id) %>% 
-  summarise(testoLemm = paste(lemma, collapse = " ")) %>% 
-  ungroup()
+# welcome sentences cleaning
 
 df_1_1 <- df_1_1 %>% 
-  mutate(n_w = str_count(testoLemm, whole_word(one_or_more(WRD)))) %>% 
-  mutate(testoLemm = if_else(n_w < 4, "", testoLemm)) %>% 
-  select(-n_w)
+  filter(!str_detect(descr_clean, whole_word(case_insensitive("benvenut") %R% one_or_more(WRD)))) %>% 
+  filter(!str_detect(descr_clean, whole_word(case_insensitive("benarrivat") %R% one_or_more(WRD)))) %>% 
+  filter(!str_detect(descr_clean, whole_word(case_insensitive(or("ciao", "salve", "buongiorno", "buonasera")))))
 
-# JOINING EXAMS ID WITCH SENTENCES ID -------------------------------------
+# interrogative and exclamative sentences cleaning
+
+df_1_1 <- df_1_1 %>% 
+  filter(!str_detect(descr_clean, or("\\!", "\\?")))
+
+# course composition sentences cleaning
+
+df_1_1 <- df_1_1 %>% 
+  filter(!str_detect(descr_clean, whole_word(case_insensitive(or("corso", "corsi", "classe", "ore", "ora", 
+                                                                 "classi", "lezione", "lezioni")))))
+
+# call to action sentences cleaning
+
+df_1_1 <- df_1_1 %>% 
+  filter(!str_detect(descr_clean, whole_word(case_insensitive("iscriv") %R% one_or_more(WRD)))) %>% 
+  filter(!str_detect(descr_clean, whole_word(case_insensitive("registrat") %R% one_or_more(WRD)))) %>% 
+  filter(!str_detect(descr_clean, whole_word(case_insensitive("affrettat") %R% one_or_more(WRD)))) %>%
+  filter(!str_detect(descr_clean, whole_word(case_insensitive(or("dammi retta", "datemi retta", "rimborsato", "che aspetti"
+                                                                 , "che aspettate", "gratuitamente")))))
+
+# course content and other stuff cleaning
+
+df_1_1 <- df_1_1 %>% 
+  filter(!str_detect(descr_clean, whole_word(case_insensitive(or("slides", "download", "esercizi", "esercitazioni",
+                                                                 "esercitazione")))))
+
+# take the negative version of cleaned dataset in order to check the cleaning fairness
+
+df_dirty_1 <- df_1 %>% 
+  left_join(df_1_1 %>% select(id, id_sentence) %>% mutate(tag = 1), by = c("id", "id_sentence")) %>% 
+  filter(is.na(tag))
 
 
-id_df_1 <- df_1 %>% 
-  select(-descr)
- 
+# DESCR COLUMN TEXT CLEANING BY LENGHT -----------------------------------
 
-df_clean <- left_join(df_1_1, id_df_1, by = c("doc_id" = "id_sentence"))
+# ultimately because of there is too much noise in the description dataset we try to go to the hard way
+# and we hypotize that short sentences in this dataset have little or none chanches to be informative
+# to set the trashold we can measure the average lenght of skill column that is much more clean and fair
+
+hist(df_2_2 %>% mutate(skill_clean = str_length(skill_clean)) %>% pull(skill_clean), breaks = 100)
+
+# the distribution is strongly positive skewed
+
+mean(df_2_2 %>% mutate(skill_clean = str_length(skill_clean)) %>% pull(skill_clean))
+
+# in order to confirm our hypoytesis we can measure deleted sentences avg lenght it must be shorter thant 70
+
+mean(df_dirty_1 %>% mutate(descr_clean = str_length(descr_clean)) %>% pull(descr_clean))
+
+# so the lenght trashold must be between 70 or 100
+
+df_1_1 <- df_1_1 %>% 
+  filter(str_length(descr_clean) > 100)
+
+#we need a new negative dataset                                                                              
+df_dirty_1 <- df_1 %>% 
+  left_join(df_1_1 %>% select(id, id_sentence) %>% mutate(tag = 1), by = c("id", "id_sentence")) %>% 
+  filter(is.na(tag))
+
+# MERGING COLUMN ----------------------------------------------------------
+
+# merge skill and descr column uniforming their names 
+
+df_1_1 <- df_1_1 %>% 
+  select(id, descr, skill, descr_clean) %>% 
+  rename(merged_sentences = descr_clean )
+
+df_2_2 <- df_2_2 %>% 
+  select(id, descr, skill, skill_clean) %>% 
+  rename(merged_sentences = skill_clean )
+
+# filter empty rows and assign a new consistent sentences id
+
+df_3 <- bind_rows(df_1_1, df_2_2) %>% 
+  filter(str_detect(merged_sentences, WRD) & !is.na(merged_sentences)) %>% 
+  arrange(id) %>% 
+  group_by(id) %>% 
+  mutate(sentences_id = row_number()) %>% 
+  ungroup() 
+
 
 # OUTPUT GENERATION -------------------------------------------------------
 
+write_rds(df_3, "Intermediate/df_3.rds")
 
-write_rds(df_clean, "Intermediate/df_clean.rds")
+write_rds(df_1_1, "Intermediate/df_1_1.rds")
+
+write_rds(df_2_2, "Intermediate/df_2_2.rds")
+
+write_rds(df_dirty_1, "Intermediate/df_dirty_1.rds")
+
+write_rds(df_dirty_2, "Intermediate/df_dirty_2.rds")
